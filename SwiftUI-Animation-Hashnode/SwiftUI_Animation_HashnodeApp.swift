32eacef297981b7ab0d24f505a225d51551be2c0
adamkif <adam.sirkif@gmail.com>
2021-09-08 08:56:24 +0100

    lesson two

--- SwiftUI-Animation-Hashnode/SwiftUI_Animation_HashnodeApp.swift
+++ SwiftUI-Animation-Hashnode/SwiftUI_Animation_HashnodeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_Animation_HashnodeApp: App {
     var body: some Scene {
         WindowGroup {
-            LessonOne()
+            LessonTwo()
         }
     }
 }
