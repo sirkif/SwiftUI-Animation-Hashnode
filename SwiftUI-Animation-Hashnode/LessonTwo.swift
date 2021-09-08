7e6c98c66123554e4ebc39d7ba7e084e0e88326d
adamkif <adam.sirkif@gmail.com>
2021-09-08 23:36:33 +0100

    add duration to our animation

--- SwiftUI-Animation-Hashnode/LessonTwo.swift
+++ SwiftUI-Animation-Hashnode/LessonTwo.swift
@@ -9,24 +9,25 @@ import SwiftUI
 
 struct LessonTwo: View {
     
-    @State private var spacingAnimation = false
+    @State private var spacingAnimation: CGFloat = 15
     
     var body: some View {
         
-        HStack(spacing: spacingAnimation ? CGFloat(15) : CGFloat(5)) {
+        // spacing: spacingAnimation ? CGFloat(15) : CGFloat(5)
+        HStack(spacing: spacingAnimation){
             
             Capsule(style: .continuous)
                 .fill(Color(#colorLiteral(red: 0.02352941176, green: 0.8392156863, blue: 0.6274509804, alpha: 1)))
                 .frame(width: 10, height: 60)
-            
+
             Capsule(style: .continuous)
                 .fill(Color(#colorLiteral(red: 1, green: 0.8196078431, blue: 0.4, alpha: 1)))
                 .frame(width: 10, height: 40)
-            
+
             Capsule(style: .continuous)
                 .fill(Color(#colorLiteral(red: 0.937254902, green: 0.2784313725, blue: 0.4352941176, alpha: 1)))
                 .frame(width: 10, height: 60)
-            
+
             Capsule(style: .continuous)
                 .fill(Color(#colorLiteral(red: 1, green: 0.8196078431, blue: 0.4, alpha: 1)))
                 .frame(width: 10, height: 40)
@@ -35,9 +36,11 @@ struct LessonTwo: View {
                 .fill(Color(#colorLiteral(red: 0.02352941176, green: 0.8392156863, blue: 0.6274509804, alpha: 1)))
                 .frame(width: 10, height: 60)
         }
-        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
+        .animation(.easeInOut(duration: 4))
         .onAppear {
-            spacingAnimation.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            spacingAnimation = 5
+            }
         }
     }
 }
